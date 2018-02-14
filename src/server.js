2e57438c982182909fd70e6e64b5a6ed266e6e09
bdurb <brian@rpmgreenville.com>
2018-02-14 16:55:32 -0500

    set up id var

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const STATUS_USER_ERROR = 422;
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [];
+let id = 0;
 
 const server = express();
 // to enable parsing of json bodies for post requests
